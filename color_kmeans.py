8f6a7cda38701fee88e1505a11a35087833c075a
Hoang Cong Tuan <hoangcongtuan@Hoangs-MacBook-Pro.local>
2019-01-29 16:19:48 +0700

    generate output file ;

--- color_kmeans.py
+++ color_kmeans.py
@@ -8,51 +8,81 @@ from matplotlib.patches import Rectangle
 import argparse
 import utils
 import cv2
+import os
+import time
 from colormap import hex2rgb
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
-ap.add_argument("-c", "--clusters", required = True, type = int,
-	help = "# of clusters")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+# ap.add_argument("-c", "--clusters", required = True, type = int,
+# 	help = "# of clusters")
+# args = vars(ap.parse_args())
 
-# load the image and convert it from BGR to RGB so that
-# we can dispaly it with matplotlib
-image = cv2.imread(args["image"])
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.style.use('dark_background')
+res_folder = 'ResourceFolder'
+list_dir = [f for f in os.listdir(res_folder) if not f.startswith('.')]
+output = open('output.txt', 'w')
 
-# show our image
-plt.figure(figsize=(4, 4))
-plt.axis("off")
-plt.imshow(image)
+for dir in list_dir:
+    list_file = [f for f in os.listdir(res_folder + '/' + dir) if not f.startswith('.')]
+    index = 0
+    print(f'processing dir: {dir}\n')
+    plt.ion()
+    for f in list_file:
+        file_path = res_folder + '/' + dir + '/' + f
+        image = cv2.imread(file_path)
+        image = cv2.resize(image,None,fx=0.5,fy=0.5)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.reshape((image.shape[0] * image.shape[1], 3))
+        clt = KMeans(n_clusters = 3)
+        clt.fit(image)
 
-# reshape the image to be a list of pixels
-image = image.reshape((image.shape[0] * image.shape[1], 3))
+        # build a histogram of clusters and then create a figure
+        # representing the number of pixels labeled to each color
+        hist = utils.centroid_histogram(clt)
+        # bar = utils.plot_colors(hist, clt.cluster_centers_)
+        primaryColor, secondaryColor = utils.findPri_Sec(hist, clt.cluster_centers_)
+        print(f'{file_path} primary = {primaryColor}, secondary = {secondaryColor}')
 
-# cluster the pixel intensities
-clt = KMeans(n_clusters = args["clusters"])
-clt.fit(image)
+        output.write(f'{file_path} {primaryColor} {secondaryColor}\n')
 
-# build a histogram of clusters and then create a figure
-# representing the number of pixels labeled to each color
-hist = utils.centroid_histogram(clt)
-# bar = utils.plot_colors(hist, clt.cluster_centers_)
+output.close()
 
-primaryColor, secondaryColor = utils.findPri_Sec(hist, clt.cluster_centers_)
-
-print(f'primary = {primaryColor}, secondary = {secondaryColor}')
-
-# show our color bar
-# plt.figure()
-# plt.axis("off")
-
-# plt.imshow(bar)
+# # load the image and convert it from BGR to RGB so that
+# # we can dispaly it with matplotlib
+# image = cv2.imread(args["image"])
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # plt.style.use('dark_background')
 
-plt.figure(figsize=(4, 4))
-currentAxis = plt.gca()
-currentAxis.add_patch(Rectangle((0.2 - 0.1, 0.5 - 0.1), 0.2, 0.2, fill = True, color = primaryColor))
-currentAxis.add_patch(Rectangle((0.5 - 0.1, 0.5 - 0.1), 0.2, 0.2, fill = True, color = secondaryColor))
-plt.show()
+# # show our image
+# plt.figure(figsize=(4, 4), num = 1)
+# plt.axis("off")
+# plt.imshow(image)
+
+# # reshape the image to be a list of pixels
+
+# # cluster the pixel intensities
+# clt = KMeans(n_clusters = args["clusters"])
+# clt.fit(image)
+
+# # build a histogram of clusters and then create a figure
+# # representing the number of pixels labeled to each color
+# hist = utils.centroid_histogram(clt)
+# # bar = utils.plot_colors(hist, clt.cluster_centers_)
+
+# primaryColor, secondaryColor = utils.findPri_Sec(hist, clt.cluster_centers_)
+
+# print(f'primary = {primaryColor}, secondary = {secondaryColor}')
+
+# # show our color bar
+# # plt.figure()
+# # plt.axis("off")
+
+# # plt.imshow(bar)
+# # plt.style.use('dark_background')
+
+# plt.figure(figsize=(4, 4), num = 2)
+# currentAxis = plt.gca()
+# currentAxis.add_patch(Rectangle((0.2 - 0.1, 0.5 - 0.1), 0.2, 0.2, fill = True, color = primaryColor))
+# currentAxis.add_patch(Rectangle((0.5 - 0.1, 0.5 - 0.1), 0.2, 0.2, fill = True, color = secondaryColor))
+# plt.show()
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 from colormap import rgb2hex
 
+white_color = np.array([255, 255, 255])
+
 def centroid_histogram(clt):
 	# grab the number of different clusters and create a histogram
 	# based on the number of pixels assigned to each cluster
@@ -16,12 +18,10 @@ def centroid_histogram(clt):
 	# return the histogram
 	return hist
 
-
 def findPri_Sec(hist, centroids):
 	hist, centroids = zip(*sorted(zip(hist, centroids), reverse = True))
 	dist2white = np.array([])
 	hexColor = np.array([])
-	white_color = np.array([255, 255, 255])
 
 	for (percent, color) in zip(hist, centroids):
 		# plot the relative percentage of each cluster
@@ -31,7 +31,7 @@ def findPri_Sec(hist, centroids):
 		dist = np.linalg.norm(white_color - int_color_np)
 		dist2white = np.append(dist2white, dist.astype('uint32'))
 
-	dist2white ,hist, hexColor = zip(*sorted(zip(dist2white, hist, hexColor), reverse = True))
+	dist2white, hist, hexColor = zip(*sorted(zip(dist2white, hist, hexColor), reverse = True))
 
 	colorCount = len(centroids)
 
